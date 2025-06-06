eb738e66a76b7cf6332b8fa1b039c086794c7816
Amircolgang <parham38385747@gmail.com>
2025-06-06 14:05:27 +0330

    url

--- server.js
+++ server.js
@@ -1,21 +1,25 @@
 const http = require("node:http");
 const fs = require("fs");
+const url = require("url")
 const servere = http.createServer((req, res) => {
-  if (req.url == "/api/users" && req.method == "GET") {
-    fs.readFile("db.json", (err, db) => {
-        if(err){
-            throw err
-        }
-        const data = JSON.parse(db);
-        console.log(data)
-
-      res.writeHead(200, { "Content-Type": "application/json" });
-      res.write(JSON.stringify(data.users));
-      res.end();
-    });
-  }
+    if (req.url == "/api/users" && req.method == "GET") {
+        fs.readFile("db.json", (err, db) => {
+            if (err) {
+                throw err
+            }
+            const data = JSON.parse(db);
+            console.log(data)
+            res.writeHead(200, { "Content-Type": "application/json" });
+            res.write(JSON.stringify(data.users));
+            res.end();
+        });
+    } else if (req.method == "DELETE") {
+        const paresedURL = url.parse(req.url , true)
+        console.log(paresedURL.query.id)
+        res.end("Test")
+    }
 });
 
 servere.listen(3000, () => {
-  console.log("Server Run On Server 3000");
+    console.log("Server Run On Server 3000");
 });
