7b9c0dc9f64863e558ea4dff5b2f01f300e492f6
Amircolgang <parham38385747@gmail.com>
2025-06-08 16:06:28 +0330

    fix remove books

--- server.js
+++ server.js
@@ -14,22 +14,35 @@ const servere = http.createServer((req, res) => {
             res.write(JSON.stringify(data.users));
             res.end();
         });
-    } else if (req.method == "DELETE") {
+    } else if (req.method == "DELETE" && req.url == "/api/books") {
         const paresedURL = url.parse(req.url, true)
         const bookID = paresedURL.query.id
-        const newBooks = db.books.filter(book => book.id != bookID
-        )
-        fs.writeFile(
-            "db.json",
-            JSON.stringify({ ...db, books: newBooks }),
-            (err) => {
-                if (err) {
-                    throw err
+        const newBooks = db.books.filter(book => book.id != bookID)
+
+        console.log(newBooks.length === db.books.length)
+        console.log(db.books.length)
+        console.log(newBooks.length)
+        if (newBooks.length === db.books.length) {
+            res.writeHead(401, { "Content-Type": "application/json" })
+            res.write("Book Not Found :(")
+            res.end()
+        }
+        else {
+            fs.writeFile(
+                "db.json",
+                JSON.stringify({ ...db, books: newBooks }),
+                (err) => {
+                    if (err) {
+                        throw err
+                    }
+                    res.writeHead(200 , {"Content-Type" : "appliaction/json"})
+                    res.write(JSON.stringify({ massage: "Booke Removed" }))
+                    res.end()
                 }
-                res.write(JSON.stringify({ massage: "Booke Removed" }))
-            }
-        )
-        res.end("Test")
+            )
+
+        }
+        res.end("Remove Is Resolve")
     }
 });
 
